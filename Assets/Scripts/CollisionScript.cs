4ea7ef495a725a997fcc1a04a3ed0d47956897a5
hiroki-yui0718 <xxx_yyy_www_1234@yahoo.co.jp>
2019-11-20 14:42:18 +0900

    Improve the README file

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour
+{
+    static int kasanTime = 0;
+    int count = 1;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Cube") || collision.gameObject.CompareTag("Cube1") || collision.gameObject.CompareTag("Cube2"))
+        {
+            if (kasanTime - int.Parse(GameObject.Find("Time").GetComponent<Text>().text) < 30 && kasanTime != 0)
+            {
+                count++;
+                score += 50;
+                score += (10 * count);
+                int now = int.Parse(GameObject.Find("SCORE").GetComponent<Text>().text);
+                GameObject.Find("SCORE").GetComponent<Text>().text = (now + score).ToString();
+                Destroy(collision.gameObject);
+                kasanTime = int.Parse(GameObject.Find("Time").GetComponent<Text>().text);
+                Debug.Log("1");
+            }
+            else
+            {
+                score += 50;
+                int now = int.Parse(GameObject.Find("SCORE").GetComponent<Text>().text);
+                GameObject.Find("SCORE").GetComponent<Text>().text = (now + score).ToString();
+                Destroy(collision.gameObject);
+                kasanTime = int.Parse(GameObject.Find("Time").GetComponent<Text>().text);
+                Debug.Log("2");
+            }
+        }
+
+
+        Debug.Log("Hit"); // ログを表示する
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour { 
+ public float speed = 20; // 動く速さ
+
+    private Rigidbody rb; // Rididbody
+    private int time;
+
+    public GameObject bullet;
+
+    // 弾丸発射点
+    public Transform muzzle;
+
+    // 弾丸の速度
+    public float speed2 = 1000;
+
+    void Start()
+    {
+        // Rigidbody を取得
+        rb = GetComponent<Rigidbody>();
+    }
+    public void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        // カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        // カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Ridigbody に力を与えて玉を動かす
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            // 弾丸の複製
+            GameObject bullets = Instantiate(bullet) as GameObject;
+
+            Vector3 force;
+
+            force = this.gameObject.transform.forward * speed2;
+
+            // Rigidbodyに力を加えて発射
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+
+            // 弾丸の位置を調整
+            bullets.transform.position = muzzle.position;
+        }
+    }
+
+
+}
